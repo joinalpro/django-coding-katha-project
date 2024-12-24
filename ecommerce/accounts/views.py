34834a6ab80a1c74f214fe76e5daadcb838753d2
joinalzx <joinalzx@gmail.com>
2024-12-24 15:11:41 +0600

    authentication done

--- ecommerce/accounts/views.py
+++ ecommerce/accounts/views.py
@@ -1,19 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from core.models import *
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 def user_login(request):
-    pass
-
-
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username = username, password = password)
+        if user is not None:
+            login(request,user)
+            return redirect('/')
+    return render(request, 'accounts/login.html')
 
 def user_register(request):
-    pass
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+        phone = request.POST.get("phone_field")
+        # print(username, email)
+        if password == confirm_password:
+            if User.objects.filter(username = username).exists():
+                print("Username already exists")
+                return redirect('user_register')
+            else:
+                if User.objects.filter(email = email).exists():
+                    print("Email already exists")
+                    return redirect('user_register')
+                else:
+                    user = User.objects.create_user(username = username, email=email, password=password)
+                    user.save()
+                    data = Customer(user = user, phone_field = phone)
+                    data.save()
+        
+                    
+                    # Code for Login of user will come here
+                    our_user = authenticate(username = username, password = password)
+                    if our_user is not None:
+                        login(request, user)
+                        return redirect('/')
+        else:
+            print("Error Here...")
+            return redirect('user_register')
+    return render(request, 'accounts/register.html')
 
 
 
 def user_logout(request):
-    pass
-
+    logout(request)
+    return redirect('/')
 
