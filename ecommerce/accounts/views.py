f2cdd6e4301322a63fd78267431aa25c80c17f68
joinalzx <joinalzx@gmail.com>
2024-12-24 13:01:46 +0600

    accounts app created for user login and register

---
+++ ecommerce/accounts/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from core.models import *
+# Create your views here.
+
+def user_login(request):
+    pass
+
+
+
+def user_register(request):
+    pass
+
+
+
+def user_logout(request):
+    pass
+
+
--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +121,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
